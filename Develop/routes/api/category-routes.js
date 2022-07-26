5f9c7b26978922c9ad7d2b3893c1f0bf133231cf
Jaryodd Carter <jaryodd@gmail.com>
2022-07-26 13:51:38 -0600

    category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,56 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  let result = await Category.findAll({
+    include: [
+      {
+      model:Product
+    },
+    ]
+  })
+  res.status(200).json(result)
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const result = await Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product
+      },
+    ]
+  })
+  res.status(200).json(result)
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  const newCategory = await Category.create(req.body)
+  res.status(200).json(newCategory)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  const update = await Category.update(req.body, {
+    where: {
+      id:req.params.id
+    }
+  })
+  res.status(200).json(update)
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  const delCategory = await Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  res.status(200).json(delCategory)
 });
 
 module.exports = router;
